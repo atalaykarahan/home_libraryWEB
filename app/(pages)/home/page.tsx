d01951334e8c145e0c837fae2110ba16cb28f7d0
atalaykarahan <krhnatalay@gmail.com>
2023-12-23 11:51:37 +0300

    login sayfası tasarımsal olarak yapıldı

---
+++ app/(pages)/home/page.tsx
@@ -0,0 +1,18 @@
+import Banner from "./banner/banner";
+import HomeBanner from "./banner/banner";
+import Brand from "./brand/brand";
+import ByCategory from "./byCategory/byCategory";
+import LastBook from "./lastBook/lastBook";
+
+const Home = () => {
+  return (
+    <>
+    <Banner/>
+      <Brand/>
+      <LastBook/>
+      <ByCategory/>
+    </>
+  );
+};
+
+export default Home;
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,7 @@
 // import Home from "./pages/home/home";
-import Banner from "./pages/home/banner/banner";
-import Brand from "./pages/home/brand/brand";
-import ByCategory from "./pages/home/byCategory/byCategory";
-import LastBook from "./pages/home/lastBook/lastBook";
-
+import HomePage from "./(pages)/home/page"
 export default function Home() {
   return (
-    <div>
-      <Banner/>
-      <Brand/>
-      <LastBook/>
-      <ByCategory/>
-    </div>
+    <HomePage/>
   );
 }
