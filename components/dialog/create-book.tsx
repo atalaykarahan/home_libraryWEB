f8ce29a1a288088dc772869973ddc2eafc6012be
atalaykarahan <krhnatalay@gmail.com>
2024-02-08 19:46:31 +0300

    publisher select inputu demo seviyede düzeltildi

--- components/dialog/create-book.tsx
+++ components/dialog/create-book.tsx
@@ -50,9 +50,15 @@ const CreateBook: React.FC<CreateCategoryProps> = ({
   openModal,
   closeModal,
 }) => {
+  //#region default page value
+
+  //#region use state
+  //publisher
   const [openPublisher, setOpenPublisher] = useState(false);
   const [value, setValue] = useState<number>(0);
-  const [publishers, setPublishers] = useState<Publisher[]>([]);
+  const [publishers, setPublishers] = useState<Option[]>([]);
+
+  //category
   const [categories, setCategories] = useState<Option[]>([]);
 
   useEffect(() => {
@@ -62,72 +68,126 @@ const CreateBook: React.FC<CreateCategoryProps> = ({
 
   const fetchData = async () => {
     try {
+      //#region publisher query
       const resPublisher = await getAllPublisherClient();
 
       if (resPublisher.status !== 200) {
         throw new Error("publisher ile ilgili bir hata oluştu");
       }
 
-      const responsePublisher = resPublisher.data;
-      setPublishers(responsePublisher);
+      const responsePublisher: Publisher[] = resPublisher.data;
+      console.table(responsePublisher);
 
-      const resCategory = await getCategories();
-      if(resCategory.status !== 200){
-        throw new Error("category ile ilgili bir hata oluştu");
-      }
-      const responseCategory:Category[] = resCategory.data;
-
-      console.table(responseCategory);
-      const transformedCategories = responseCategory.map(category => ({
-        label: category.category_name,
-        value: category.category_id.toString()
+      const transformedPublishers = responsePublisher.map((publisher) => ({
+        label: publisher.publisher_name,
+        value: publisher.publisher_id?.toString() ?? "0",
       }));
 
-      if(transformedCategories){
-        setCategories(transformedCategories);
+      if (transformedPublishers) {
+        setPublishers(transformedPublishers);
       }
 
+      //#endregion
 
+      //#region category query
+      const resCategory = await getCategories();
+      if (resCategory.status !== 200) {
+        throw new Error("category ile ilgili bir hata oluştu");
+      }
+      const responseCategory: Category[] = resCategory.data;
 
+      const transformedCategories = responseCategory.map((category) => ({
+        label: category.category_name,
+        value: category.category_name,
+        key: category.category_id.toString(),
+      }));
 
-
-
-
-
-
+      if (transformedCategories) {
+        setCategories(transformedCategories);
+      }
+      //#endregion
     } catch (error) {
-      console.log("publisher try&catch hata -> ", error);
+      console.log("fetchData try&catch hata -> ", error);
     }
   };
 
+  // const form = useForm<z.infer<typeof CreateBookSchema>>({
+  //   resolver: zodResolver(CreateBookSchema),
+  //   defaultValues: {
+  //     book_title: "",
+  //     author_id: 0,
+  //     publisher_id: 0,
+  //     status_id: 0,
+  //     book_summary: "",
+  //   },
+  // });
+
+  // const onSubmit = (values: z.infer<typeof CreateBookSchema>) => {
+  //   console.log(values);
+  // };
+
+  //#endregion
+
+  // const OPTIONS: Option[] = [
+  //   { label: "nextjs", value: "Nextjs", key: "1" },
+  //   { label: "React", value: "react", key: "3" },
+  //   { label: "Remix", value: "remix", key: "4" },
+  //   { label: "Vite", value: "vite" },
+  //   { label: "Nuxt", value: "nuxt" },
+  //   { label: "Vue", value: "vue" },
+  //   { label: "Svelte", value: "svelte" },
+  //   { label: "Angular", value: "angular" },
+  //   { label: "Ember", value: "ember", disable: true },
+  //   { label: "Gatsby", value: "gatsby", disable: true },
+  //   { label: "Astro", value: "astro" },
+  // ];
+
+  // const optionSchema = z.object({
+  //   label: z.string(),
+  //   value: z.string(),
+  //   key:z.string().optional(),
+  //   disable: z.boolean().optional(),
+  // });
+
+  // const FormSchema = z.object({
+  //   frameworks: z.array(optionSchema).min(1),
+  // });
+
+  // const languages = [
+  //   { label: "English", value: "en" },
+  //   { label: "French", value: "fr" },
+  //   { label: "German", value: "de" },
+  //   { label: "Spanish", value: "es" },
+  //   { label: "Portuguese", value: "pt" },
+  //   { label: "Russian", value: "ru" },
+  //   { label: "Japanese", value: "ja" },
+  //   { label: "Korean", value: "ko" },
+  //   { label: "Chinese", value: "zh" },
+  // ] as const
+
   const form = useForm<z.infer<typeof CreateBookSchema>>({
     resolver: zodResolver(CreateBookSchema),
-    defaultValues: {
-      book_title: "",
-      author_id: 0,
-      publisher_id: 0,
-      status_id: 0,
-      book_summary: "",
-    },
   });
 
-  const OPTIONS: Option[] = [
-    { label: 'nextjs', value: 'nextjs' },
-    { label: 'React', value: 'react' },
-    { label: 'Remix', value: 'remix' },
-    { label: 'Vite', value: 'vite' },
-    { label: 'Nuxt', value: 'nuxt' },
-    { label: 'Vue', value: 'vue' },
-    { label: 'Svelte', value: 'svelte' },
-    { label: 'Angular', value: 'angular' },
-    { label: 'Ember', value: 'ember', disable: true },
-    { label: 'Gatsby', value: 'gatsby', disable: true },
-    { label: 'Astro', value: 'astro' },
-  ];
+  const [loading, setLoading] = useState(false);
 
-  const onSubmit = (values: z.infer<typeof CreateBookSchema>) => {
-    console.log(values);
-  };
+  function onSubmit(data: z.infer<typeof CreateBookSchema>) {
+    // setLoading(true);
+    console.log(data);
+
+    // setTimeout(() => {
+    //   setLoading(false);
+    //   toast({
+    //     title: 'Your submitted data',
+    //     description: (
+    //       <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+    //         <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+    //       </pre>
+    //     ),
+    //   });
+    // }, 500);
+  }
+
   return (
     <Dialog open={openModal} onOpenChange={() => closeModal()}>
       <DialogContent className="sm:max-w-[425px]">
@@ -135,19 +195,121 @@ const CreateBook: React.FC<CreateCategoryProps> = ({
           <DialogTitle>Kitap Ekle</DialogTitle>
           <DialogDescription>açıklama kısmı</DialogDescription>
         </DialogHeader>
-
-        <MultipleSelector
-        defaultOptions={categories}
-        placeholder="Select frameworks you like..."
-        emptyIndicator={
-          <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-            no results found.
-          </p>
-        }
-      />
-
-
         <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <div className="space-y-4">
+              {/* book name */}
+              <FormField
+                control={form.control}
+                name="book_title"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Kitap Adı</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        placeholder="ömer seyfettinin kaşağı"
+                        type="text"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              {/* publisher  */}
+              <FormField
+                control={form.control}
+                name="publisher_id"
+                render={({ field }) => (
+                  <FormItem className="flex flex-col">
+                    <FormLabel>Yayınevi</FormLabel>
+                    <Popover>
+                      <PopoverTrigger asChild>
+                        <FormControl>
+                          <Button
+                            variant="outline"
+                            role="combobox"
+                            className={cn(
+                              "w-[200px] justify-between",
+                              !field.value && "text-muted-foreground"
+                            )}
+                          >
+                            {field.value
+                              ? publishers.find((p) => p.value === field.value)
+                                  ?.label
+                              : "Select language"}
+                            <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+                          </Button>
+                        </FormControl>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-[200px] p-0">
+                        <Command>
+                          <CommandInput
+                            placeholder="Search framework..."
+                            className="h-9"
+                          />
+                          <CommandEmpty>No framework found.</CommandEmpty>
+                          <CommandGroup>
+                            {publishers.map((p) => (
+                              <CommandItem
+                                value={p.label}
+                                key={p.value}
+                                onSelect={() => {
+                                  form.setValue("publisher_id", p.value);
+                                }}
+                              >
+                                {p.label}
+                                <CheckIcon
+                                  className={cn(
+                                    "ml-auto h-4 w-4",
+                                    p.value === field.value
+                                      ? "opacity-100"
+                                      : "opacity-0"
+                                  )}
+                                />
+                              </CommandItem>
+                            ))}
+                          </CommandGroup>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                  </FormItem>
+                )}
+              />
+
+              {/* categories */}
+              <FormField
+                control={form.control}
+                name="categories"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Kategoriler</FormLabel>
+                    <FormControl>
+                      <MultipleSelector
+                        value={field.value}
+                        onChange={field.onChange}
+                        defaultOptions={categories}
+                        placeholder="Kitaba uygun kategorileri seçiniz.."
+                        emptyIndicator={
+                          <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
+                            kategori bulunamadı.
+                          </p>
+                        }
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <Button type="submit" className="w-full">
+                Oluştur
+              </Button>
+            </div>
+          </form>
+        </Form>
+
+        {/* <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
             <div className="space-y-4">
               <FormField
@@ -266,12 +428,12 @@ const CreateBook: React.FC<CreateCategoryProps> = ({
               />
             </div>
             {/* <FormError message={errorMessage} />
-            <FormSuccess message={successMessage} /> */}
+            <FormSuccess message={successMessage} /> 
             <Button type="submit" className="w-full">
               Oluştur
             </Button>
           </form>
-        </Form>
+        </Form> */}
       </DialogContent>
     </Dialog>
   );
--- schemas/book.ts
+++ schemas/book.ts
@@ -1,9 +1,24 @@
 import * as z from "zod";
 
+const optionSchema = z.object({
+  label: z.string(),
+  value: z.string(),
+  key:z.string().optional(),
+  disable: z.boolean().optional(),
+});
+
+// const FormSchema = z.object({
+//   frameworks: z.array(optionSchema).min(1),
+// });
+
+
+
+
 export const CreateBookSchema = z.object({
   book_title: z.string(),
-  author_id: z.number(),
-  publisher_id: z.number(),
-  status_id: z.number(),
-  book_summary: z.string(),
+  // author_id: z.number(),
+  publisher_id: z.string(),
+  // status_id: z.number(),
+  // book_summary: z.string(),
+  categories: z.array(optionSchema).min(1),
 });
