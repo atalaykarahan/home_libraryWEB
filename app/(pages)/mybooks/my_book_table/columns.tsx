8890c5c21bc88c252e73ba57f4bc1f26bd692fed
atalaykarahan <krhnatalay@gmail.com>
2024-02-27 20:59:00 +0300

    kitaplarım sayfasına her bir row için select özelliği getirildi

--- app/(pages)/mybooks/my_book_table/columns.tsx
+++ app/(pages)/mybooks/my_book_table/columns.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { ColumnDef } from "@tanstack/react-table";
+import { TbDots } from "react-icons/tb";
+
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export type MyBookTableModel = {
   book_id: number;
@@ -26,5 +35,31 @@ export const columns: ColumnDef<MyBookTableModel>[] = [
   {
     accessorKey: "status",
     header: "Durumu",
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const myBook = row.original
+ 
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <TbDots className="h-4 w-4"/>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuItem>Aç</DropdownMenuItem>
+            <DropdownMenuItem>Düzenle</DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(myBook.book_title)}
+            >
+              Sil
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
   },
 ];
