8b44cc4ae5bbb6a241215a91caedbe7802c29c66
atalaykarahan <krhnatalay@gmail.com>
2024-04-21 16:08:06 +0300

    footer ve anasayfadaki linkler target blank yapıldı ve eksiklikler giderildi

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -16,22 +16,33 @@ const Footer = () => {
           <div className="w-full md:w-1/3 mb-6 md:mb-0">
             <h3 className="text-base font-bold">Ben kimim</h3>
             <p className="mb-2">
-            Bu internet sitesi, kendimi geliştirmek, yeni teknolojileri öğrenmek ve evimdeki kitapların log bilgilerini tutmak üzere tasarlanmıştır. İçerisinde SMTP, S3 Bucket gibi teknolojileri bulundurmaktadır. Kaynak kodları için GitHub hesabımı ziyaret edebilirsiniz.
+              Bu internet sitesi, kendimi geliştirmek, yeni teknolojileri
+              öğrenmek ve evimdeki kitapların log bilgilerini tutmak üzere
+              tasarlanmıştır. İçerisinde SMTP, S3 Bucket gibi teknolojileri
+              bulundurmaktadır. Kaynak kodları için GitHub hesabımı ziyaret
+              edebilirsiniz.
             </p>
             <p>
-              &copy; {new Date().getFullYear()} Karahan Kitaplık. Tüm hakları saklıdır.
+              &copy; {new Date().getFullYear()} Karahan Kitaplık. Tüm hakları
+              saklıdır.
             </p>
           </div>
           <FooterList>
             <h3 className="text-base font-bold">Beni Takip Et</h3>
             <div className="flex gap-2">
-              <Link href="#">
+              <Link
+                href="https://www.linkedin.com/in/atalay-karahan-050985250/"
+                target="_blank"
+              >
                 <FaLinkedin size={24} />
               </Link>
-              <Link href="#">
+              <Link
+                href="https://www.instagram.com/krhnatalay/"
+                target="_blank"
+              >
                 <AiFillInstagram size={24} />
               </Link>
-              <Link href="#">
+              <Link href="https://github.com/atalaykarahan" target="_blank">
                 <AiOutlineGithub size={24} />
               </Link>
             </div>
