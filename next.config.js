58a56a29e37df07ce8be6508ac2c9bb88a9c4922
atalay karahan <krhnatalay@gmail.com>
2024-04-22 09:50:30 +0300

    Update next.config.js
    image optimization devre dışı bırakılmaya çalışıldı yoksa vercel payment istiyor

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  module.exports = {
+  images: {
+    unoptimized: true,
+  },
+}
   images: {
     domains: [
       "tailwindui.com",
